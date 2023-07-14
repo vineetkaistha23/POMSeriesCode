5ca9bd8afdbf58492e184863cd34402dc8039b63
vineetkaistha23 <vineetkaisthapm@gmail.com>
2023-07-15 09:01:27 +1200

    new changes added.

--- src/main/java/com/qa/opencart/pages/Orderpage.java
+++ src/main/java/com/qa/opencart/pages/Orderpage.java
@@ -4,4 +4,14 @@ public class Orderpage {
 	
 	int i=10;
 	
+	public void getPrice()
+	{
+		System.out.println("Get Price");
+	}
+	
+	public void getOderDetails()
+	{
+		System.out.println("Get oder details");
+	}
+	
 }
